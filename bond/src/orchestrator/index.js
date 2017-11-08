e12367a92eddc102d5547c1f3b1b778d81f9563a
Attila Bagossy <bagossyattila@outlook.com>
2017-11-08 22:42:33 +0100

    Now calling git directly

--- bond/src/orchestrator/index.js
+++ bond/src/orchestrator/index.js
@@ -94,7 +94,7 @@ const Orchestrator = {
         return this.deps.steps.createDirectory.create(cloneDirectory);
     },
     cloneRepository(context) {
-        return this.deps.steps.cloneRepository.clone(context.build.repoUri, context.build.branch, cloneDirectory);
+        return this.deps.steps.cloneRepository.clone(context.build.repoUri, context.build.commitHash, cloneDirectory);
     },
     async readBaseImage(context) {
         const filename = path.join(cloneDirectory, 'brocan.hjson');
