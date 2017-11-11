76e8f5f5be046285048e38812d414a5d0cc5a445
Attila Bagossy <bagossyattila@outlook.com>
2017-11-11 20:46:22 +0100

    Init'd Origins

---
+++ components/services/origins/src/config.js
@@ -0,0 +1,28 @@
+const convict = require('convict');
+const logger = require('./logger').child({ component: 'config' });
+
+
+const config = convict({
+    env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'test'],
+        default: 'production',
+        env: 'NODE_ENV'
+    },
+    nats: {
+        uri: {
+            doc: 'The URI of the NATS network.',
+            format: String,
+            default: 'nats://nats:4222',
+            env: 'NATS_URI'
+        }
+    }
+});
+
+
+logger.debug('Loading configuration with schema');
+logger.debug(config.getSchema());
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
