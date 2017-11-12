b8012cd273a62eb3820185736d33a40a6e400aa8
Attila Bagossy <bagossyattila@outlook.com>
2017-11-12 21:16:23 +0100

    Added the correct topics to GitHub Transformer

--- components/services/transformer/github/src/index.js
+++ components/services/transformer/github/src/index.js
@@ -8,36 +8,29 @@ const Transformer = require('./transformer');
 
 Messaging.ready(function messagingReady() {
     Messaging.add({
-        topic: 'build',
-        role: 'transform',
+        topic: 'build.transform',
         origin: 'github',
     }, async function transform(request) {
         logger.info('Received a GitHub transformation request',);
 
-        logger.debug(request.payload);
+        logger.debug(request.webhookRequest);
 
-        Transformer.transform(request.payload)
-            .then(buildRequest => publishNewBuild(request.payload, buildRequest))
+        Transformer.transform(request.webhookRequest)
+            .then(buildRequest => publishNewBuild(request.webhookRequest, buildRequest))
             .catch(transformationFailure);
 
         return;
     });
 
-    function publishNewBuild(originalRequest, buildRequest) {
+    function publishNewBuild(webhookRequest, buildRequest) {
         return new Promise((resolve, reject) => {
             Messaging.act({
-                topic: 'build',
+                topic: 'build.info',
                 role: 'new',
-                request: originalRequest,
+                webhookRequest,
                 buildRequest,
 
                 pubsub$: true
-            }, err => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
             });
         });
     }
